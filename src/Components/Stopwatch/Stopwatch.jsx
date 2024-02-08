94f46b7e485a1598b7734f5bbb88407512229083
1 <a1@1s-MacBook-Pro.local>
2024-02-08 14:03:06 +0530

    completed

--- src/Components/Stopwatch/Stopwatch.jsx
+++ src/Components/Stopwatch/Stopwatch.jsx
@@ -4,11 +4,15 @@ import { ReactComponent as PlayButton } from "../../Assets/play_circle.svg";
 import { ReactComponent as PauseButton } from "../../Assets/pause_circle.svg";
 import { ReactComponent as StopButton } from "../../Assets/stop_circle.svg";
 import { ReactComponent as VolumeButton } from "../../Assets/volume_up.svg";
+import { ReactComponent as DarkmodeButton } from "../../Assets/dark_mode.svg";
 
 const Stopwatch = () => {
   const [time, setTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isPlaying, setIsPlaying] = useState(false);
   const timer = useRef(null);
+  const audioref = useRef(null);
 
   const startTimer = () => {
     setIsRunning(true);
@@ -22,13 +26,12 @@ const Stopwatch = () => {
     clearInterval(timer.current);
     setIsRunning(false);
     setTime(0);
-  }
+  };
 
   const pauseTimer = () => {
     clearInterval(timer.current);
     setIsRunning(false);
-  }
-
+  };
 
   function showTime(time) {
     const hour = Math.floor(time / 3600000);
@@ -36,25 +39,47 @@ const Stopwatch = () => {
     const sec = Math.floor((time % 60000) / 1000);
     const remainingMilliseconds = time % 1000;
 
-    return `${doubleZero(hour)}:${doubleZero(min)}:${doubleZero(sec)}.${doubleZero(remainingMilliseconds)}`;
+    return `${doubleZero(hour)}:${doubleZero(min)}:${doubleZero(
+      sec
+    )}.${doubleZero(remainingMilliseconds)}`;
   }
 
   const doubleZero = (value) => (value < 10 ? `0${value}` : value);
 
+  const toggleDarkMode = () => {
+    setIsDarkMode((prev) => !prev);
+  };
 
+  function alarmPlay (){
+    if(!isPlaying){
+        audioref.current.play();
+    }
+    else{
+        audioref.current.pause();
+    }
+
+    setIsPlaying((prev)=>!prev);
+  }
 
 
   return (
-    <div className={styles.wrapper}>
-      <div className={styles.timer}>
-        <h2>{showTime(time)}</h2>
+    <div>
+      <div className={`${styles.wrapper} ${isDarkMode ? styles.darkMode : ""}`}>
+      <DarkmodeButton onClick={toggleDarkMode} />
+        <div className={styles.timer}>
+          <h2>{showTime(time)}</h2>
+        </div>
+        <div className={`${styles.controller} ${isDarkMode ? styles.controllerchange : ""}`}>
+          {!isRunning ? (
+            <PlayButton onClick={startTimer} className={styles.play} />
+          ) : (
+            <PauseButton onClick={pauseTimer} className={styles.play} />
+          )}
+          <StopButton onClick={stopTimer} className={styles.pause} />
+          <VolumeButton onClick={alarmPlay} className={styles.volume} />
+        </div>
       </div>
-      <div className={styles.controller}>
-        <PlayButton onClick={startTimer} className={styles.play} />
-        <PauseButton onClick={pauseTimer} className={styles.play}/>
-        <StopButton onClick={stopTimer} className={styles.pause} />
-        <VolumeButton className={styles.volume} />
-      </div>
+      <audio ref={audioref} src={require("../../Assets/Audio/perfect_alarm.mp3")} />
     </div>
   );
 };
